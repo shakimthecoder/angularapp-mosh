d33797b806d0a5b9ecc6a8749f6692e77c338d0d
Shakim Harris <sharris@gls.us.com>
2019-07-26 10:25:25 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
   title = 'onlineapp';
+
+  constructor () {
+    function log(message){
+    console.log('Hello' + message);
+  }
+  var message = 'This works';
+  
+  log(message);
+} 
+  
 }
