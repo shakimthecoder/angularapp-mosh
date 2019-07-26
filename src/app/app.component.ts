a59a24f77e17d69e746abdcb0f928d820d4e4cdb
Shakim Harris <sharris@gls.us.com>
2019-07-26 11:48:15 -0400

    fixed old code and creatd new obj Point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,46 +12,20 @@ export class AppComponent {
   title = 'onlineapp';
 
   constructor () {
-  let a: number;
-  let b: string;
-  let c: boolean;
-  let d: any;
-  let e: number[] = [1, 2, 3];
-  let f: any[] = [1, false, 'shakim'];
-
-  const ColorRed= 0;
-  const ColorBlue = 1;
-  const ColorGreen = 2;
-
-  enum Color { Red = 0, Green = 2, Blue = 1 };
-  let backgroundColor = Color.Red
-
-  let message;
-  message='abc';
-  let endsWithC =  (<string>message).endsWith('c');
-  let alternativeWay = (message as string).endsWith('c');
-
-  let log = function(message){
-    console.log(message);
-  }
-
-  let DoLog = () =>
-    console.log('message');
-  
-  DoLog();
-
   class Point {
     x: number;
     y: number;
 
     draw() {
      console.log('X:' + this.x + ', Y:', + this.y);
-     }
-     getDistance(another: Point){
+      }
+
+     getDistance(another: Point) {
        //...
        }
     }
-  }
-    let point: Point;
+  
+    let point: new Point();
     point.draw();
-
+  }
+}
