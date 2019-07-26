84262a446db3f3a7c3fa1ba4d4014078de6a5892
Shakim Harris <sharris@gls.us.com>
2019-07-26 10:57:19 -0400

    asserted type in alternate way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,7 @@ export class AppComponent {
   let message;
   message='abc';
   let endsWithC =  (<string>message).endsWith('c');
+  let alternativeWay = (message as string).endsWith('c');
 
 } 
   
