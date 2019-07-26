cf5644fda0f2eacc254e428a09d58269d1cfdd97
Shakim Harris <sharris@gls.us.com>
2019-07-26 12:27:31 -0400

    added get method to return x value inside class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,23 +10,20 @@ import { Component } from '@angular/core';
 
 export class PointComponent {
   title = 'onlineapp';
-  private x: 0;
-  private y: 0;
 
   constructor (private x?: number, private y?: number) {
-    this.x = x;
-    this.y =y;
+  }
 
-  }
-  
-  Point: number {
-    x: 0;
-    y:0; 
-  }
+
   draw() {
     console.log('X:' + this.x + ', Y:', + this.y);
      }
+
+  getX() {
+    return this.x;
+     }
   }
-   point = 0;
-   let point = new point();
+
+   let point = new point(1, 2);
+   let x = point.getX();
    point.draw();
