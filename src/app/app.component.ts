bc881c7ad6a91515145f14e356664a00625b5bb8
Shakim Harris <sharris@gls.us.com>
2019-07-26 12:11:38 -0400

    made param optional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,20 @@ import { Component } from '@angular/core';
 
 
 
-export class AppComponent {
+export class PointComponent {
   title = 'onlineapp';
 
-  constructor () {
-  class Point {
-    x: number;
-    y: number;
+  constructor (x?: number, y?: number) {
+    this.x = x;
+    this.y =y;
 
-    draw() {
-     console.log('X:' + this.x + ', Y:', + this.y);
-      }
-
-     getDistance(another: Point) {
-       //...
-       }
-    }
-  
-    let point: new Point();
-    point.x = 1;
-    point.y = 6;
-    point.draw();
   }
-}
+  
+
+  draw() {
+    console.log('X:' + this.x + ', Y:', + this.y);
+     }
+  }
+ 
+   let point = new Point();
+   point.draw();
